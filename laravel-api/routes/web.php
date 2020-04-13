de633e772da09328a093116e82c6c411aa0c9b39
gustavo torregrosa <gustavo.torregrosa@gmail.com>
2020-04-13 14:14:14 -0300

    final

--- laravel-api/routes/web.php
+++ laravel-api/routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "actual-sales api test made by gustavo torregosa...";
 });
 
 // Auth::routes();
